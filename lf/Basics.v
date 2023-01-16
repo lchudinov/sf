6f12c45a8a85518cc1adea88032334169feef990
Leonty Chudinov <leonty.chudinov@gmail.com>
2023-01-16 08:00:43 +0500

    Nat3 done
    Signed-off-by: Leonty Chudinov <leonty.chudinov@gmail.com>

--- lf/Basics.v
+++ lf/Basics.v
@@ -255,3 +255,46 @@ Example test_factorial1: (factorial 3) = 6.
 Proof. simpl. reflexivity. Qed.
 Example test_factorial2: (factorial 5) = (mult 10 12).
 Proof. simpl. reflexivity. Qed.
+
+Fixpoint eqb (n m: nat) : bool :=
+  match n with
+  | O => match m with
+        | O => true
+        | S _ => false
+        end
+  | S n' => match m with
+        | O => false
+        | S m' => eqb n' m'
+        end
+  end.
+  
+Fixpoint leb (n m: nat) : bool :=
+  match n with
+  | O => true
+  | S n' => match m with
+        | O => false
+        | S m' => leb n' m'
+        end
+  end.
+
+  Example test_leb1: leb 2 2 = true.
+  Proof. simpl. reflexivity. Qed.
+  Example test_leb2: leb 2 4 = true.
+  Proof. simpl. reflexivity. Qed.
+  Example test_leb3: leb 4 2 = false.
+  Proof. simpl. reflexivity. Qed.
+  
+  Notation "x =? y" := (eqb x y) (at level 70) : nat_scope.
+  Notation "x <=? y" := (leb x y) (at level 70) : nat_scope.
+  Example test_leb3': (4 <=? 2) = false.
+  Proof. simpl. reflexivity. Qed.
+  
+Definition ltb (n m : nat) : bool :=
+  negb (m <=? n).
+Notation "x <? y" := (ltb x y) (at level 70) : nat_scope.
+Example test_ltb1: (ltb 2 2) = false.
+Proof. simpl. reflexivity. Qed.
+Example test_ltb2: (ltb 2 4) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_ltb3: (ltb 4 2) = false.
+Proof. simpl. reflexivity. Qed.
