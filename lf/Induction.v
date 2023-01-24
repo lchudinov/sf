8a429ded73dbb80a0dd21b0e42b0c009b54728be
Leonty Chudinov <leonty.chudinov@gmail.com>
2023-01-24 08:14:37 +0500

    More induction
    Signed-off-by: Leonty Chudinov <leonty.chudinov@gmail.com>

--- lf/Induction.v
+++ lf/Induction.v
@@ -27,3 +27,28 @@ Proof.
   - reflexivity.
   - simpl. rewrite -> IHn'. reflexivity.
 Qed.
+
+Theorem minus_n_n: forall n,
+  n - n = 0.
+Proof.
+  intros n. induction n as [| k].
+   - reflexivity.
+   - simpl. rewrite -> IHk. reflexivity.
+Qed.
+
+Theorem plus_n_Sm: forall n m : nat,
+  S (n + m) = n + S m.
+Proof.
+  intros n m. induction n as [| n'].
+  - simpl. reflexivity.
+  - simpl. rewrite IHn'. reflexivity.
+Qed.
+
+Theorem add_comm : forall a b : nat,
+  a + b = b + a.
+Proof.
+  intros a b. induction a as [| a'].
+  - simpl. rewrite add_0_r. reflexivity.
+  - simpl. rewrite IHa'. rewrite plus_n_Sm. reflexivity.
+Qed.
+  
