0c7a476cf75823074653d14ca049b4867577333d
Leonty Chudinov <leonty.chudinov@gmail.com>
2023-01-25 08:28:36 +0500

    Proof within proof

--- lf/Induction.v
+++ lf/Induction.v
@@ -51,4 +51,84 @@ Proof.
   - simpl. rewrite add_0_r. reflexivity.
   - simpl. rewrite IHa'. rewrite plus_n_Sm. reflexivity.
 Qed.
-  
+
+Theorem mult_0_plus' : forall n m : nat,
+  (n + 0 + 0) * m = n * m.
+Proof.
+  intros n m.
+  assert (H: n + 0 + 0 = n).
+  { 
+    (*Set Printing Parentheses.*)
+    rewrite add_comm.
+    simpl.
+    rewrite add_comm.
+    simpl.
+    reflexivity.
+  }
+  rewrite -> H.
+  reflexivity.
+Qed.
+
+Theorem plus_rearrange_firsttry : forall n m p q : nat,
+  (n + m) + (p + q) = (m + n) + (p + q).
+Proof.
+  intros n m p q.
+  (* Set Printing Parentheses. *)
+  rewrite add_comm.
+Abort.
+
+Theorem plus_rearrange : forall n m p q : nat,
+  (n + m) + (p + q) = (m + n) + (p + q).
+Proof.
+  intros n m p q.
+  assert (H: n + m = m + n).
+  { rewrite add_comm. reflexivity. }
+  rewrite H.
+  reflexivity. 
+Qed.
+
+Theorem mul_0_r : forall n : nat,
+  n * 0 = 0.
+Proof.
+  intros n. induction n as [|n' IHn'].
+  - reflexivity.
+  - simpl. rewrite IHn'. reflexivity.
+Qed.
+
+Theorem add_assoc : forall n m p : nat,
+  n + (m + p) = (n + m) + p.
+Proof.
+  intros n m p. induction n as [|n'].
+  - reflexivity.
+  - simpl. rewrite IHn'. reflexivity.
+Qed.
+
+Fixpoint double (n:nat) :=
+  match n with
+  | O => O
+  | S n' => S (S (double n'))
+  end.
+
+Lemma double_plus : forall n, double n = n + n .
+Proof.
+  intros n. induction n as [|n'].
+  - reflexivity.
+  - simpl. rewrite IHn'. rewrite plus_n_Sm. reflexivity.
+Qed.
+
+Theorem eqb_refl : forall n : nat,
+  (n =? n) = true.
+Proof.
+  intros n. induction n as [|n'].
+  - simpl. reflexivity.
+  - simpl. rewrite IHn'. reflexivity.
+Qed.
+
+Theorem even_S : forall n : nat,
+  even (S n) = negb (even n).
+Proof.
+  intros n. induction n as [|n'].
+  - simpl. reflexivity.
+  - rewrite -> IHn'. rewrite negb_involutive. simpl. reflexivity.
+Qed.
+
