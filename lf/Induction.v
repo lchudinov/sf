7325f8d4c2324263af333e9c8c808e1b481326ed
Leonty Chudinov <leonty.chudinov@gmail.com>
2023-01-31 08:55:09 +0500

    mul_comm

--- lf/Induction.v
+++ lf/Induction.v
@@ -132,3 +132,116 @@ Proof.
   - rewrite -> IHn'. rewrite negb_involutive. simpl. reflexivity.
 Qed.
 
+Theorem add_assoc' : forall n m p : nat,
+  n + (m + p) = (n + m) + p.
+Proof.
+  intros n m p. induction n as [|n' IHn']. reflexivity.
+  simpl. rewrite IHn'. reflexivity.
+Qed.
+
+Theorem add_assoc'' : forall n m p : nat,
+  n + (m + p) = (n + m) + p.
+Proof.
+  intros n m p. induction n as [|n' IHn'].
+  - (* n = 0 *)
+    reflexivity.
+  - (* n = S n' *)
+    simpl. rewrite IHn'. reflexivity.
+(** - _Theorem_: For any [n], [m] and [p],
+      n + (m + p) = (n + m) + p.
+    _Proof_: By induction on [n].
+    - First, suppose [n = 0]. We must show that
+        0 + (m + p) = (0 + m) + p.
+      This follows directly from the definition of [+].
+    - Next, suppose [n = S n'], where
+      n' + (m + p) = (n' + m) + p.
+    We must now show that  
+      (S n') + (m + p) = ((S n') + m) + p.
+    By the definition of [+], this follows from
+      S (n' + (m + p)) = S((n' + m) + p),
+    which is immediate from the induction hypothesis. _Qed_.
+*)
+Qed.
+
+Theorem add_comm' : forall a b : nat,
+  a + b = b + a.
+Proof.
+  intros a b. induction a as [| a'].
+  - simpl. rewrite add_0_r. reflexivity.
+  - simpl. rewrite IHa'. rewrite plus_n_Sm. reflexivity.
+(** - _Theorem_: For any [a] and [b],
+      a + b = b + a.
+    _Proof_: By induction on [a].
+    - First, suppose [a = 0]. We must show that
+        0 + b = b + 0.
+      This follows directly from the definition of [+].
+    - Next, suppose [a = S a'], where
+      a' + b = b + 'a.
+    We must now show that  
+      (S a') + b = b + (S a').
+    By the definition of [+], this follows from
+      S (a' + b) = S (b + a').
+    which is immediate from the induction hypothesis. _Qed_.
+*)
+Qed.
+
+Theorem add_shuffle3 : forall n m p : nat,
+  n + (m + p) = m + (n + p).
+Proof.
+  intros n m p.
+  rewrite add_assoc.
+  assert (H1: m + (n + p) = m + n + p).
+  { rewrite add_assoc. reflexivity. }
+  assert (H2: n + m = m + n).
+  { rewrite add_comm. reflexivity. }
+  rewrite H1.
+  rewrite <- H2.
+  reflexivity.
+Qed.
+Theorem  mul_n_0: forall n : nat,
+  n * 0 = 0.
+Proof.
+  induction n.
+  - reflexivity.
+  Admitted.
+
+Lemma n_k_1 : forall n k : nat,
+  n * (1 + k) = n + n * k.
+Proof.
+  intros n k.
+  induction n as [|n' IHn'].
+  - simpl. reflexivity.
+  - simpl. rewrite add_assoc.
+    assert (n' + k = k + n').
+    { rewrite add_comm. reflexivity. }
+    rewrite H.
+    rewrite <- add_assoc.
+    rewrite <- IHn'.
+    Set Printing Parentheses.
+    assert (1 + k = S k).
+    { rewrite plus_1_n. reflexivity. }
+    rewrite H0.
+    reflexivity.
+  Qed.
+    
+Theorem mul_comm : forall m n : nat,
+  m * n = n * m.
+Proof.
+  intros n m.
+  induction n as [|k IHk'].
+  - simpl.
+    assert (m * 0 = 0).
+    induction m.
+    -- rewrite mul_0_r. reflexivity.
+    -- simpl. rewrite IHm. reflexivity.
+    -- rewrite H. reflexivity.
+  - assert (m * (S k) = m * (1 + k)).
+    { rewrite <- plus_1_n. reflexivity. }
+    rewrite H.
+    rewrite n_k_1.
+    rewrite <- IHk'.
+    rewrite <- plus_1_n.
+    simpl.
+    reflexivity.
+Qed.
+    
