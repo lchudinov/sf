5023baab6edea7a3a81c327cb55246b0b202a25e
Leonty Chudinov <leonty.chudinov@gmail.com>
2023-05-21 22:09:10 +0500

    Finish Maps

--- lf/Maps.v
+++ lf/Maps.v
@@ -99,6 +99,73 @@ Proof.
     + reflexivity.
 Qed.
 
+Definition partial_map (A : Type) := total_map (option A).
+Definition empty {A : Type} : partial_map A := t_empty None.
+Definition update {A : Type} (m : partial_map A) (x : string) (v : A) := 
+  (x !-> Some v; m).
+
+Notation "x '|->' v  ';' m" :=
+  (update m x v)
+  (at level 100, v at next level, right associativity).
+
+Notation "x '|->' v" := (update empty x v)
+  (at level 100).
+
+Definition examplepmap :=
+  ("Church" |-> true ; "Turing" |-> false).
+
+Lemma apply_empty : forall (A : Type) (x : string),
+  @empty A x = None.
+Proof. intros. unfold empty. rewrite t_apply_empty. reflexivity. Qed.
+
+Lemma update_eq : forall (A : Type) (m : partial_map A) x v,
+  (x |-> v ; m) x = Some v.
+Proof.
+  intros. unfold update. rewrite t_update_eq. reflexivity.
+Qed.
+
+Theorem update_neq : forall (A : Type) (m : partial_map A) x1 x2 v,
+  x2 <> x1 ->
+  (x2 |-> v ; m) x1 = m x1.
+Proof.
+  intros. unfold update. rewrite t_update_neq.
+  - reflexivity.
+  - apply H.
+Qed.
+
+Lemma update_shadow : forall (A : Type) (m : partial_map A) x v1 v2,
+  (x |-> v2 ; x |-> v1 ; m) = (x |-> v2 ; m).
+Proof. intros A m x v1 v2. unfold update. rewrite t_update_shadow. reflexivity. Qed.
+
+Theorem update_same : forall (A : Type) (m : partial_map A) x v,
+  m x = Some v ->
+  (x |-> v ; m) = m.
+Proof. intros A m x v H. unfold update. rewrite <- H. apply t_update_same. Qed.
+
+Theorem update_permute : forall (A : Type) (m : partial_map A) x1 x2 v1 v2,
+  x2 <> x1 ->
+  (x1 |-> v1 ; x2 |-> v2 ; m) = (x2 |-> v2 ; x1 |-> v1 ; m).
+Proof. intros A m x1 x2 v1 v2. unfold update. apply t_update_permute. Qed.
+
+Definition includedin {A : Type} (m m' : partial_map A) :=
+  forall x v, m x = Some v -> m' x = Some v.
+
+Lemma includedin_update : forall (A : Type) (m m' : partial_map A) (x : string) (vx : A),
+  includedin m m' ->
+  includedin (x |-> vx ; m) (x |-> vx ; m').
+Proof.
+  unfold includedin.
+  intros A m m' x vx H.
+  intros y vy.
+  destruct (eqb_spec x y) as [Hxy | Hxy].
+  - rewrite Hxy. rewrite update_eq. rewrite update_eq. intro H1. apply H1.
+  - rewrite update_neq.
+    + rewrite update_neq.
+      * apply H.
+      * apply Hxy.
+    + apply Hxy.
+Qed.
+
 
 
   
