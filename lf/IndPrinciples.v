5d30fa92e7c57af95602f7682a01c2413d445b3b
Leonty Chudinov <leonty.chudinov@gmail.com>
2023-06-05 08:32:22 +0500

    More done

--- lf/IndPrinciples.v
+++ lf/IndPrinciples.v
@@ -1,3 +1,6 @@
+Set Warnings "-notation-overridden,-parsing,-deprecated-hint-without-locality".
+From LF Require Export ProofObjects.
+
 Check nat_ind :
   forall P : nat -> Prop,
     P 0 ->
@@ -162,3 +165,91 @@ apply nat_ind.
   intros n IHn.
   unfold P_m0r in IHn. unfold P_m0r. simpl. apply IHn.
 Qed.
+
+Theorem add_assoc' : forall n m p : nat,
+  n + (m + p) = (n + m) + p.
+Proof.
+  (* ...we first introduce all 3 variables into the context,
+     which amounts to saying "Consider an arbitrary n, m, and
+     p..." *)
+  intros n m p.
+  (* ...We now use the induction tactic to prove P n (that
+     is, n + (m + p) = (n + m) + p) for _all_ n,
+     and hence also for the particular n that is in the context
+     at the moment. *)
+  induction n as [| n'].
+  - (* n = O *) reflexivity.
+  - (* n = S n' *)
+    simpl. rewrite -> IHn'. reflexivity.
+Qed.
+
+Theorem add_0_r : forall n:nat, n + 0 = n.
+Proof.
+  intros n. induction n as [| n' IHn'].
+  - (* n = 0 *) reflexivity.
+  - (* n = S n' *) simpl. rewrite -> IHn'. reflexivity. Qed.
+
+Theorem add_comm'' : forall n m : nat,
+  n + m = m + n.
+Proof.
+  induction n as [| n'].
+  - (* n = O *) intros m. rewrite -> add_0_r. reflexivity.
+  - (* n = S n' *) intros m. simpl. rewrite -> IHn'.
+    rewrite <- plus_n_Sm. reflexivity.
+Qed.
+
+Definition P_add_comm (n m:nat) : Prop :=
+  n + m = m + n.
+
+Definition P_add_comm' : nat -> nat -> Prop :=
+  fun n =>  fun m => n + m = m + n.
+
+Theorem P_add_comm'' : forall (n m:nat),
+P_add_comm n m.
+Proof.
+  induction n as [|n'].
+- (* n = O *) intros m. unfold P_add_comm. simpl. rewrite add_0_r. reflexivity. 
+- (* n = S n' *)
+  intros m. unfold P_add_comm in IHn'. unfold P_add_comm. simpl. rewrite <- plus_n_Sm. rewrite IHn'. reflexivity.
+Qed.
+
+Print ev.
+
+Inductive ev' : nat -> Prop :=
+  | ev'_0 : ev' 0
+  | ev'_2 : ev' 2
+  | ev'_sum n m (Hn : ev' n) (Hm : ev' m) : ev' (n + m).
+  
+Theorem ev_ev' : forall n, ev n -> ev' n.
+Proof.
+  apply ev_ind.
+  - (* ev_0 *)
+    apply ev'_0.
+  - (* ev_SS *)
+    intros m Hm IH.
+    apply (ev'_sum 2 m).
+    + apply ev'_2.
+    + apply IH.
+  Qed.
+
+Inductive le1 : nat -> nat -> Prop :=
+| le1_n : forall n, le1 n n
+| le1_S : forall n m, (le1 n m) -> (le1 n (S m)).
+Notation "m <=1 n" := (le1 m n) (at level 70).
+
+Inductive le2 (n : nat) : nat -> Prop :=
+  | le2_n : le2 n n
+  | le2_S m (H : le2 n m) : le2 n (S m).
+Notation "m <=2 n" := (le2 m n) (at level 70).
+
+Check le1_ind :
+  forall P : nat -> nat -> Prop,
+    (forall n : nat, P n n) ->
+    (forall n m : nat, n <=1 m -> P n m -> P n (S m)) ->
+    forall n n0 : nat, n <=1 n0 -> P n n0.
+
+Check le2_ind :
+  forall (n : nat) (P : nat -> Prop),
+    P n ->
+    (forall m : nat, n <=2 m -> P m -> P (S m)) ->
+    forall n0 : nat, n <=2 n0 -> P n0.
