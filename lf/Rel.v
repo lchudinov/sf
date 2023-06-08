5a959d8351427ebbe1fa4074695c7077bbf61be8
Leonty Chudinov <leonty.chudinov@gmail.com>
2023-06-08 07:41:46 +0500

    Rel done

--- lf/Rel.v
+++ lf/Rel.v
@@ -169,3 +169,77 @@ Proof.
     - split.
       + (* antisym *) apply le_antisymmetric.
       + (* transitive. *) apply le_trans. Qed.
+
+Inductive clos_refl_trans {A: Type} (R: relation A) : relation A :=
+| rt_step x y (H : R x y) : clos_refl_trans R x y
+| rt_refl x : clos_refl_trans R x x
+| rt_trans x y z
+      (Hxy : clos_refl_trans R x y)
+      (Hyz : clos_refl_trans R y z) :
+      clos_refl_trans R x z.
+      
+Theorem next_nat_closure_is_le : forall n m,
+  (n <= m) <-> ((clos_refl_trans next_nat) n m).
+Proof.
+  intros n m. split.
+  - (* -> *)
+    intro H. induction H.
+    + (* le_n *) apply rt_refl.
+    + (* le_S *)
+      apply rt_trans with m. apply IHle. apply rt_step.
+      apply nn.
+  - (* <- *)
+    intro H. induction H.
+    + (* rt_step *) inversion H. apply le_S. apply le_n.
+    + (* rt_refl *) apply le_n.
+    + (* rt_trans *)
+      apply le_trans with y.
+      apply IHclos_refl_trans1.
+      apply IHclos_refl_trans2.
+Qed.
+
+Inductive clos_refl_trans_1n {A : Type}
+                             (R : relation A) (x : A)
+                             : A -> Prop :=
+  | rt1n_refl : clos_refl_trans_1n R x x
+  | rt1n_trans (y z : A)
+      (Hxy : R x y) (Hrest : clos_refl_trans_1n R y z) :
+      clos_refl_trans_1n R x z.
+
+      Lemma rsc_trans :
+      forall (X:Type) (R: relation X) (x y z : X),
+          clos_refl_trans_1n R x y  ->
+          clos_refl_trans_1n R y z ->
+          clos_refl_trans_1n R x z.
+    Proof.
+      intros X R x y z H1 H2.
+      induction H1 as [y | x y y' Hxy _ IH].
+      - apply H2.
+      - apply rt1n_trans with y. apply Hxy. apply (IH H2).
+    Qed.
+      
+Lemma rsc_R : forall (X:Type) (R:relation X) (x y : X),
+  R x y -> clos_refl_trans_1n R x y.
+Proof.
+  intros X R x y H.
+  apply rt1n_trans with y. apply H. apply rt1n_refl. Qed.
+Theorem rtc_rsc_coincide :
+  forall (X:Type) (R: relation X) (x y : X),
+    clos_refl_trans R x y <-> clos_refl_trans_1n R x y.
+Proof.
+  split.
+  - intros H.
+    induction H as [x y H | x | x y z _ IH1 _ IH2].
+    + apply rsc_R. apply H.
+    + apply rt1n_refl.
+    + apply rsc_trans with y. apply IH1. apply IH2.
+  - intros H.
+    induction H as [x | x y z H _ IH].
+    + apply rt_refl.
+    + apply rt_trans with y.
+      * apply rt_step. apply H.
+      * apply IH.
+Qed.
+
+
+  
