35eb428b9491d11c3440dc4386727fce5fe7a402
Leonty Chudinov <leonty.chudinov@gmail.com>
2023-06-13 08:28:40 +0500

    Finish Expressions With Variables

--- lf/Imp.v
+++ lf/Imp.v
@@ -300,7 +300,9 @@ Inductive aevalR : aexp -> nat -> Prop :=
       (AMult e1 e2) ==> (n1 * n2)
 
   where "e '==>' n" := (aevalR e n) : type_scope.
-  
+
+Reserved Notation "e '==>b' b" (at level 90, left associativity).
+
 Inductive bevalR : bexp -> bool -> Prop :=
   | E_BTrue : bevalR BTrue true
   | E_BFalse : bevalR BFalse false
@@ -327,8 +329,218 @@ Inductive bevalR : bexp -> bool -> Prop :=
     bevalR a1 b1 ->
     bevalR a2 b2 ->
     bevalR (BAnd a1 a2) (andb b1 b2)
-  .
+
+where "e '==>b' b" := (bevalR e b) : type_scope.
+
+Theorem aeval_iff_aevalR : forall a n,
+  (a ==> n) <-> aeval a = n.
+Proof.
+ split.
+ - (* -> *)
+   intros H.
+   induction H; simpl.
+   + (* E_ANum *)
+     reflexivity.
+   + (* E_APlus *)
+     rewrite IHaevalR1. rewrite IHaevalR2. reflexivity.
+   + (* E_AMinus *)
+     rewrite IHaevalR1. rewrite IHaevalR2. reflexivity.
+   + (* E_AMult *)
+     rewrite IHaevalR1. rewrite IHaevalR2. reflexivity.
+ - (* <- *)
+   generalize dependent n.
+   induction a;
+      simpl; intros; subst.
+   + (* ANum *)
+     apply E_ANum.
+   + (* APlus *)
+     apply E_APlus.
+     * apply IHa1. reflexivity.
+     * apply IHa2. reflexivity.
+   + (* AMinus *)
+     apply E_AMinus.
+     * apply IHa1. reflexivity.
+     * apply IHa2. reflexivity.
+   + (* AMult *)
+     apply E_AMult.
+     * apply IHa1. reflexivity.
+     * apply IHa2. reflexivity.
+Qed.
+
+Theorem aeval_iff_aevalR' : forall a n,
+  (a ==> n) <-> aeval a = n.
+Proof.
+  (* WORKED IN CLASS *)
+  split.
+  - (* -> *)
+    intros H; induction H; subst; reflexivity.
+  - (* <- *)
+    generalize dependent n.
+    induction a; simpl; intros; subst; constructor;
+       try apply IHa1; try apply IHa2; reflexivity.
+Qed.
+
+Lemma beval_iff_bevalR : forall b bv,
+  b ==>b bv <-> beval b = bv.
+Proof.
+  split.
+  - intros H; induction H; simpl; subst;
+     try apply aeval_iff_aevalR in H; try apply aeval_iff_aevalR in H0; try rewrite H0; try rewrite H; try reflexivity.
+  - intros; destruct b; simpl; subst; constructor;
+    try (apply aeval_iff_aevalR; reflexivity);
+    [ | rename b1 into b | rename b2 into b ];
+    induction b; constructor;
+    try (apply aeval_iff_aevalR; reflexivity);
+    try (apply IHb);
+    try (apply IHb1);
+    try (apply IHb2).
+Qed.
+End AExp.
+
+Module aevalR_division.
+
+Inductive aexp : Type :=
+  | ANum (n : nat)
+  | APlus (a1 a2 : aexp)
+  | AMinus (a1 a2 : aexp)
+  | AMult (a1 a2 : aexp)
+  | ADiv (a1 a2 : aexp). (* <--- NEW *)
   
+Reserved Notation "e '==>' n"
+  (at level 90, left associativity).
+Inductive aevalR : aexp -> nat -> Prop :=
+| E_ANum (n : nat) :
+  (ANum n) ==> n
+| E_APlus (a1 a2 : aexp) (n1 n2 : nat) :
+  (a1 ==> n1) -> (a2 ==> n2) -> (APlus a1 a2) ==> (n1 + n2)
+| E_AMinus (a1 a2 : aexp) (n1 n2 : nat) :
+  (a1 ==> n1) -> (a2 ==> n2) -> (AMinus a1 a2) ==> (n1 - n2)
+| E_AMult (a1 a2 : aexp) (n1 n2 : nat) :
+  (a1 ==> n1) -> (a2 ==> n2) -> (AMult a1 a2) ==> (n1 * n2)
+| E_ADiv (a1 a2 : aexp) (n1 n2 n3 : nat) : (* <----- NEW *)
+  (a1 ==> n1) -> (a2 ==> n2) -> (n2 > 0) ->
+(mult n2 n3 = n1) -> (ADiv a1 a2) ==> n3
+
+where "a '==>' n" := (aevalR a n) : type_scope.
+
+End aevalR_division.
+Module aevalR_extended.
+
+Reserved Notation "e '==>' n" (at level 90, left associativity).
+Inductive aexp : Type :=
+  | AAny (* <--- NEW *)
+  | ANum (n : nat)
+  | APlus (a1 a2 : aexp)
+  | AMinus (a1 a2 : aexp)
+  | AMult (a1 a2 : aexp).
+
+Inductive aevalR : aexp -> nat -> Prop :=
+| E_Any (n : nat) :
+    AAny ==> n (* <--- NEW *)
+| E_ANum (n : nat) :
+    (ANum n) ==> n
+| E_APlus (a1 a2 : aexp) (n1 n2 : nat) :
+    (a1 ==> n1) -> (a2 ==> n2) -> (APlus a1 a2) ==> (n1 + n2)
+| E_AMinus (a1 a2 : aexp) (n1 n2 : nat) :
+    (a1 ==> n1) -> (a2 ==> n2) -> (AMinus a1 a2) ==> (n1 - n2)
+| E_AMult (a1 a2 : aexp) (n1 n2 : nat) :
+    (a1 ==> n1) -> (a2 ==> n2) -> (AMult a1 a2) ==> (n1 * n2)
+
+where "a '==>' n" := (aevalR a n) : type_scope.
+End aevalR_extended.
+
+Definition state := total_map nat.
+
+Inductive aexp : Type :=
+  | ANum (n : nat)
+  | AId (x : string) (* <--- NEW *)
+  | APlus (a1 a2 : aexp)
+  | AMinus (a1 a2 : aexp)
+  | AMult (a1 a2 : aexp).
+  
+Definition W : string := "W".
+Definition X : string := "X".
+Definition Y : string := "Y".
+Definition Z : string := "Z".
+
+Inductive bexp : Type :=
+  | BTrue
+  | BFalse
+  | BEq (a1 a2 : aexp)
+  | BNeq (a1 a2 : aexp)
+  | BLe (a1 a2 : aexp)
+  | BGt (a1 a2 : aexp)
+  | BNot (b : bexp)
+  | BAnd (b1 b2 : bexp).
+  
+Coercion AId : string >-> aexp.
+Coercion ANum : nat >-> aexp.
+
+Declare Custom Entry com.
+Declare Scope com_scope.
+
+Notation "<{ e }>" := e (at level 0, e custom com at level 99) : com_scope.
+Notation "( x )" := x (in custom com, x at level 99) : com_scope.
+Notation "x" := x (in custom com at level 0, x constr at level 0) : com_scope.
+Notation "f x .. y" := (.. (f x) .. y)
+                  (in custom com at level 0, only parsing,
+                  f constr at level 0, x constr at level 9,
+                  y constr at level 9) : com_scope.
+Notation "x + y" := (APlus x y) (in custom com at level 50, left associativity).
+Notation "x - y" := (AMinus x y) (in custom com at level 50, left associativity).
+Notation "x * y" := (AMult x y) (in custom com at level 40, left associativity).
+Notation "'true'" := true (at level 1).
+Notation "'true'" := BTrue (in custom com at level 0).
+Notation "'false'" := false (at level 1).
+Notation "'false'" := BFalse (in custom com at level 0).
+Notation "x <= y" := (BLe x y) (in custom com at level 70, no associativity).
+Notation "x > y" := (BGt x y) (in custom com at level 70, no associativity).
+Notation "x = y" := (BEq x y) (in custom com at level 70, no associativity).
+Notation "x <> y" := (BNeq x y) (in custom com at level 70, no associativity).
+Notation "x && y" := (BAnd x y) (in custom com at level 80, left associativity).
+Notation "'~' b" := (BNot b) (in custom com at level 75, right associativity).
+Open Scope com_scope.
+
+Definition example_aexp : aexp := <{ 3 + (X * 2) }>.
+Definition example_bexp : bexp := <{ true && ~(X <= 4) }>.
 
 
+Fixpoint aeval (st : state) (* <--- NEW *) (a : aexp) : nat :=
+  match a with
+  | ANum n => n
+  | AId x => st x (* <--- NEW *)
+  | <{a1 + a2}> => (aeval st a1) + (aeval st a2)
+  | <{a1 - a2}> => (aeval st a1) - (aeval st a2)
+  | <{a1 * a2}> => (aeval st a1) * (aeval st a2)
+  end.
+
+Fixpoint beval (st : state) (* <--- NEW *) (b : bexp) : bool :=
+  match b with
+  | <{true}> => true
+  | <{false}> => false
+  | <{a1 = a2}> => (aeval st a1) =? (aeval st a2)
+  | <{a1 <> a2}> => negb ((aeval st a1) =? (aeval st a2))
+  | <{a1 <= a2}> => (aeval st a1) <=? (aeval st a2)
+  | <{a1 > a2}> => negb ((aeval st a1) <=? (aeval st a2))
+  | <{~ b1}> => negb (beval st b1)
+  | <{b1 && b2}> => andb (beval st b1) (beval st b2)
+  end.
+  
+Definition empty_st := (_ !-> 0).
+
+Notation "x '!->' v" := (x !-> v ; empty_st) (at level 100).
+Example aexp1 :
+    aeval (X !-> 5) <{ 3 + (X * 2) }>
+  = 13.
+Proof. reflexivity. Qed.
+
+Example aexp2 :
+    aeval (X !-> 5 ; Y !-> 4) <{ Z + (X * Y) }>
+  = 20.
+Proof. reflexivity. Qed.
+
+Example bexp1 :
+    beval (X !-> 5) <{ true && ~(X <= 4) }>
+  = true.
+Proof. reflexivity. Qed.
 
