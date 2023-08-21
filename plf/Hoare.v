2c35396eb8ee6c078a6af371cd0bb9cce3551aed
Leonty Chudinov <leonty.chudinov@gmail.com>
2023-08-21 08:40:16 +0500

    Begin with assertions

--- plf/Hoare.v
+++ plf/Hoare.v
@@ -7,3 +7,18 @@ From Coq Require Import Arith.PeanoNat. Import Nat.
 From Coq Require Import Lia.
 From PLF Require Export Imp.
 Set Default Goal Selector "!".
+
+Definition Assertion := state -> Prop.
+
+Module ExAssertions.
+Definition assn1 : Assertion := fun st => st X <= st Y.
+(* holds for states st in which value of X less or equal of value of Y *)
+Definition assn2 : Assertion := fun st => st X = 3 \/ st X <= st Y.
+(* holds for states st in which value of X is 3 value of X less or equal of value of Y *)
+Definition assn3 : Assertion := fun st => st Z * st Z <= st X /\ ~ (((S (st Z)) * (S (st Z))) <= st X).
+(* holds for states st in which value of Z * Z is less of equal than value of X and not sqaure of the number after Z is less or equal than value of X *)
+Definition assn4 : Assertion := fun st => st Z = max (st X) (st Y).
+(* holds for states st in which value of Z equals max of values X and Y *)
+End ExAssertions.
+
+
