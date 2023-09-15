8a8b066c1258a59e570265f4fc6d5de56db7f0be
Leonty Chudinov <leonty.chudinov@gmail.com>
2023-09-15 08:29:55 +0500

    Finish Hoare.v

--- plf/Hoare.v
+++ plf/Hoare.v
@@ -775,7 +775,7 @@ Inductive ceval : state -> com -> state -> Prop :=
       st =[ c ]=> st' ->
       st' =[ while b do c end ]=> st'' ->
       st =[ while b do c end ]=> st''
-  | E_RepatFalse : forall b st st' c,
+  | E_RepeatFalse : forall b st st' c,
       beval st b = false ->
       st =[ c ]=> st' ->
       st =[ repeat c until b end ]=> st'
@@ -787,3 +787,414 @@ Inductive ceval : state -> com -> state -> Prop :=
 
 where "st '=[' c ']=>' st'" := (ceval st c st').
 
+Definition hoare_triple (P : Assertion) (c : com) (Q : Assertion): Prop :=
+  forall st st', st =[ c ]=> st' -> P st -> Q st'.
+Notation "{{ P }} c {{ Q }}" :=
+  (hoare_triple P c Q) (at level 90, c custom com at level 99).
+
+Definition ex1_repeat :=
+  <{ repeat
+       X := 1;
+       Y := Y + 1
+     until (X = 1) end }>.
+Theorem ex1_repeat_works :
+  empty_st =[ ex1_repeat ]=> (Y !-> 1 ; X !-> 1).
+Proof.
+  unfold ex1_repeat.
+  apply E_RepeatFalse.
+  + eauto.
+  + apply E_Seq with (st' := (X !-> 1)).
+    * apply E_Asgn. reflexivity.
+    * apply E_Asgn. reflexivity.
+Qed.
+
+Theorem hoare_repeat : forall P Q (b:bexp) c,
+  {{P}} c {{Q}} ->
+  (Q /\ ~ (bassn b))%assertion ->> P  ->
+  {{P}} repeat c until b end {{Q /\ (bassn b)}}.
+Proof.
+  intros P Q b c Hhoare Qnb st' st'' Heval HP.
+  (* We proceed by induction on Heval, because, in the "keep looping" case,
+     its hypotheses talk about the whole loop instead of just c. The
+     remember is used to keep the original command in the hypotheses;
+     otherwise, it would be lost in the induction. By using inversion
+     we clear away all the cases except those involving while. *)
+  remember <{repeat c until b end}> as original_command eqn:Horig.
+  
+  induction Heval;
+    try (inversion Horig; subst; clear Horig);
+    eauto; split.
+    Admitted.
+
+Example hoare_repeat_ex:
+  {{ X > 0 }}
+    repeat
+      Y := X;
+      X := X - 1
+    until X = 0 end
+  {{ X = 0 /\ Y > 0 }}.
+Proof.
+  Admitted.
+
+End RepeatExercise.
+
+Module Himp.
+Inductive com : Type :=
+  | CSkip : com
+  | CAsgn : string -> aexp -> com
+  | CSeq : com -> com -> com
+  | CIf : bexp -> com -> com -> com
+  | CWhile : bexp -> com -> com
+  | CHavoc : string -> com.
+Notation "'havoc' l" := (CHavoc l)
+                          (in custom com at level 60, l constr at level 0).
+Notation "'skip'" :=
+         CSkip (in custom com at level 0).
+Notation "x := y" :=
+         (CAsgn x y)
+            (in custom com at level 0, x constr at level 0,
+             y at level 85, no associativity).
+Notation "x ; y" :=
+         (CSeq x y)
+           (in custom com at level 90, right associativity).
+Notation "'if' x 'then' y 'else' z 'end'" :=
+         (CIf x y z)
+           (in custom com at level 89, x at level 99,
+            y at level 99, z at level 99).
+Notation "'while' x 'do' y 'end'" :=
+         (CWhile x y)
+            (in custom com at level 89, x at level 99, y at level 99).
+Inductive ceval : com -> state -> state -> Prop :=
+  | E_Skip : forall st,
+      st =[ skip ]=> st
+  | E_Asgn : forall st a1 n x,
+      aeval st a1 = n ->
+      st =[ x := a1 ]=> (x !-> n ; st)
+  | E_Seq : forall c1 c2 st st' st'',
+      st =[ c1 ]=> st' ->
+      st' =[ c2 ]=> st'' ->
+      st =[ c1 ; c2 ]=> st''
+  | E_IfTrue : forall st st' b c1 c2,
+      beval st b = true ->
+      st =[ c1 ]=> st' ->
+      st =[ if b then c1 else c2 end ]=> st'
+  | E_IfFalse : forall st st' b c1 c2,
+      beval st b = false ->
+      st =[ c2 ]=> st' ->
+      st =[ if b then c1 else c2 end ]=> st'
+  | E_WhileFalse : forall b st c,
+      beval st b = false ->
+      st =[ while b do c end ]=> st
+  | E_WhileTrue : forall st st' st'' b c,
+      beval st b = true ->
+      st =[ c ]=> st' ->
+      st' =[ while b do c end ]=> st'' ->
+      st =[ while b do c end ]=> st''
+  | E_Havoc : forall st x n,
+      st =[ havoc x ]=> (x !-> n ; st)
+
+where "st '=[' c ']=>' st'" := (ceval c st st').
+Hint Constructors ceval : core.
+
+Definition hoare_triple (P:Assertion) (c:com) (Q:Assertion) : Prop :=
+  forall st st', st =[ c ]=> st' -> P st -> Q st'.
+Hint Unfold hoare_triple : core.
+Notation "{{ P }} c {{ Q }}" := (hoare_triple P c Q)
+                                  (at level 90, c custom com at level 99)
+                                  : hoare_spec_scope.
+
+Theorem hoare_consequence_pre : forall (P P' Q : Assertion) c,
+  {{P'}} c {{Q}} ->
+  P ->> P' ->
+  {{P}} c {{Q}}.
+Proof. eauto. Qed.
+
+Definition havoc_pre (X : string) (Q : Assertion) (st : total_map nat) : Prop
+  (* REPLACE THIS LINE WITH ":= _your_definition_ ." *). Admitted.
+Theorem hoare_havoc : forall (Q : Assertion) (X : string),
+  {{ havoc_pre X Q }} havoc X {{ Q }}.
+Proof.
+  (* FILL IN HERE *) Admitted.
+
+End Himp.
+
+(* ================================================================= *)
+(** ** Assert and Assume *)
+
+(** **** Exercise: 4 stars, standard (assert_vs_assume)
+
+    In this exercise, we will extend IMP with two commands, [assert]
+    and [assume]. Both commands are ways to indicate that a certain
+    statement should hold any time this part of the program is
+    reached. However they differ as follows:
+
+    - If an [assert] statement fails, it causes the program to go into
+      an error state and exit.
+
+    - If an [assume] statement fails, the program fails to evaluate at
+      all. In other words, the program gets stuck and has no final
+      state.
+
+    The new set of commands is: *)
+
+    Module HoareAssertAssume.
+
+    Inductive com : Type :=
+      | CSkip : com
+      | CAsgn : string -> aexp -> com
+      | CSeq : com -> com -> com
+      | CIf : bexp -> com -> com -> com
+      | CWhile : bexp -> com -> com
+      | CAssert : bexp -> com
+      | CAssume : bexp -> com.
+    
+    Notation "'assert' l" := (CAssert l)
+                               (in custom com at level 8, l custom com at level 0).
+    Notation "'assume' l" := (CAssume l)
+                              (in custom com at level 8, l custom com at level 0).
+    Notation "'skip'"  :=
+             CSkip (in custom com at level 0).
+    Notation "x := y"  :=
+             (CAsgn x y)
+                (in custom com at level 0, x constr at level 0,
+                 y at level 85, no associativity).
+    Notation "x ; y" :=
+             (CSeq x y)
+               (in custom com at level 90, right associativity).
+    Notation "'if' x 'then' y 'else' z 'end'" :=
+             (CIf x y z)
+               (in custom com at level 89, x at level 99,
+                y at level 99, z at level 99).
+    Notation "'while' x 'do' y 'end'" :=
+             (CWhile x y)
+                (in custom com at level 89, x at level 99, y at level 99).
+    
+    (** To define the behavior of [assert] and [assume], we need to add
+        notation for an error, which indicates that an assertion has
+        failed. We modify the [ceval] relation, therefore, so that
+        it relates a start state to either an end state or to [error].
+        The [result] type indicates the end value of a program,
+        either a state or an error: *)
+    
+    Inductive result : Type :=
+      | RNormal : state -> result
+      | RError : result.
+    
+    (** Now we are ready to give you the ceval relation for the new language. *)
+    
+    Inductive ceval : com -> state -> result -> Prop :=
+      (* Old rules, several modified *)
+      | E_Skip : forall st,
+          st =[ skip ]=> RNormal st
+      | E_Asgn  : forall st a1 n x,
+          aeval st a1 = n ->
+          st =[ x := a1 ]=> RNormal (x !-> n ; st)
+      | E_SeqNormal : forall c1 c2 st st' r,
+          st  =[ c1 ]=> RNormal st' ->
+          st' =[ c2 ]=> r ->
+          st  =[ c1 ; c2 ]=> r
+      | E_SeqError : forall c1 c2 st,
+          st =[ c1 ]=> RError ->
+          st =[ c1 ; c2 ]=> RError
+      | E_IfTrue : forall st r b c1 c2,
+          beval st b = true ->
+          st =[ c1 ]=> r ->
+          st =[ if b then c1 else c2 end ]=> r
+      | E_IfFalse : forall st r b c1 c2,
+          beval st b = false ->
+          st =[ c2 ]=> r ->
+          st =[ if b then c1 else c2 end ]=> r
+      | E_WhileFalse : forall b st c,
+          beval st b = false ->
+          st =[ while b do c end ]=> RNormal st
+      | E_WhileTrueNormal : forall st st' r b c,
+          beval st b = true ->
+          st  =[ c ]=> RNormal st' ->
+          st' =[ while b do c end ]=> r ->
+          st  =[ while b do c end ]=> r
+      | E_WhileTrueError : forall st b c,
+          beval st b = true ->
+          st =[ c ]=> RError ->
+          st =[ while b do c end ]=> RError
+      (* Rules for Assert and Assume *)
+      | E_AssertTrue : forall st b,
+          beval st b = true ->
+          st =[ assert b ]=> RNormal st
+      | E_AssertFalse : forall st b,
+          beval st b = false ->
+          st =[ assert b ]=> RError
+      | E_Assume : forall st b,
+          beval st b = true ->
+          st =[ assume b ]=> RNormal st
+    
+    where "st '=[' c ']=>' r" := (ceval c st r).
+    
+    (** We redefine hoare triples: Now, [{{P}} c {{Q}}] means that,
+        whenever [c] is started in a state satisfying [P], and terminates
+        with result [r], then [r] is not an error and the state of [r]
+        satisfies [Q]. *)
+    
+    Definition hoare_triple
+               (P : Assertion) (c : com) (Q : Assertion) : Prop :=
+      forall st r,
+         st =[ c ]=> r  -> P st  ->
+         (exists st', r = RNormal st' /\ Q st').
+    
+    Notation "{{ P }}  c  {{ Q }}" :=
+      (hoare_triple P c Q) (at level 90, c custom com at level 99)
+      : hoare_spec_scope.
+    
+    (** To test your understanding of this modification, give an example
+        precondition and postcondition that are satisfied by the [assume]
+        statement but not by the [assert] statement. *)
+    
+    Theorem assert_assume_differ : exists (P:Assertion) b (Q:Assertion),
+           ({{P}} assume b {{Q}})
+      /\ ~ ({{P}} assert b {{Q}}).
+    (* FILL IN HERE *) Admitted.
+    
+    (** Then prove that any triple for an [assert] also works when
+        [assert] is replaced by [assume]. *)
+    
+    Theorem assert_implies_assume : forall P b Q,
+         ({{P}} assert b {{Q}})
+      -> ({{P}} assume b {{Q}}).
+    Proof.
+    (* FILL IN HERE *) Admitted.
+    
+    (** Next, here are proofs for the old hoare rules adapted to the new
+        semantics.  You don't need to do anything with these. *)
+    
+    Theorem hoare_asgn : forall Q X a,
+      {{Q [X |-> a]}} X := a {{Q}}.
+    Proof.
+      unfold hoare_triple.
+      intros Q X a st st' HE HQ.
+      inversion HE. subst.
+      exists (X !-> aeval st a ; st). split; try reflexivity.
+      assumption. Qed.
+    
+    Theorem hoare_consequence_pre : forall (P P' Q : Assertion) c,
+      {{P'}} c {{Q}} ->
+      P ->> P' ->
+      {{P}} c {{Q}}.
+    Proof.
+      intros P P' Q c Hhoare Himp.
+      intros st st' Hc HP. apply (Hhoare st st').
+      - assumption.
+      - apply Himp. assumption. Qed.
+    
+    Theorem hoare_consequence_post : forall (P Q Q' : Assertion) c,
+      {{P}} c {{Q'}} ->
+      Q' ->> Q ->
+      {{P}} c {{Q}}.
+    Proof.
+      intros P Q Q' c Hhoare Himp.
+      intros st r Hc HP.
+      unfold hoare_triple in Hhoare.
+      assert (exists st', r = RNormal st' /\ Q' st').
+      { apply (Hhoare st); assumption. }
+      destruct H as [st' [Hr HQ'] ].
+      exists st'. split; try assumption.
+      apply Himp. assumption.
+    Qed.
+    
+    Theorem hoare_seq : forall P Q R c1 c2,
+      {{Q}} c2 {{R}} ->
+      {{P}} c1 {{Q}} ->
+      {{P}} c1;c2 {{R}}.
+    Proof.
+      intros P Q R c1 c2 H1 H2 st r H12 Pre.
+      inversion H12; subst.
+      - eapply H1.
+        + apply H6.
+        + apply H2 in H3. apply H3 in Pre.
+            destruct Pre as [st'0 [Heq HQ] ].
+            inversion Heq; subst. assumption.
+      - (* Find contradictory assumption *)
+         apply H2 in H5. apply H5 in Pre.
+         destruct Pre as [st' [C _] ].
+         inversion C.
+    Qed.
+    
+    (** Here are the other proof rules (sanity check) *)
+    Theorem hoare_skip : forall P,
+         {{P}} skip {{P}}.
+    Proof.
+      intros P st st' H HP. inversion H. subst.
+      eexists. split.
+      - reflexivity.
+      - assumption.
+    Qed.
+    
+    Theorem hoare_if : forall P Q (b:bexp) c1 c2,
+      {{ P /\ b}} c1 {{Q}} ->
+      {{ P /\ ~ b}} c2 {{Q}} ->
+      {{P}} if b then c1 else c2 end {{Q}}.
+    Proof.
+      intros P Q b c1 c2 HTrue HFalse st st' HE HP.
+      inversion HE; subst.
+      - (* b is true *)
+        apply (HTrue st st').
+        + assumption.
+        + split; assumption. 
+      - (* b is false *)
+        apply (HFalse st st').
+        + assumption.
+        + split.
+          * assumption.
+          * apply bexp_eval_false. assumption.
+    Qed.
+    
+    Theorem hoare_while : forall P (b:bexp) c,
+      {{P /\ b}} c {{P}} ->
+      {{P}} while b do c end {{ P /\ ~b}}.
+    Proof.
+      intros P b c Hhoare st st' He HP.
+      remember <{while b do c end}> as wcom eqn:Heqwcom.
+      induction He;
+        try (inversion Heqwcom); subst; clear Heqwcom.
+      - (* E_WhileFalse *)
+        eexists. split.
+        + reflexivity.
+        + split; try assumption.
+          apply bexp_eval_false. assumption.
+      - (* E_WhileTrueNormal *)
+        clear IHHe1.
+        apply IHHe2.
+        + reflexivity.
+        + clear IHHe2 He2 r.
+          unfold hoare_triple in Hhoare.
+          apply Hhoare in He1.
+          * destruct He1 as [st1 [Heq Hst1] ].
+            inversion Heq; subst.
+            assumption.
+          * split; assumption.
+      - (* E_WhileTrueError *)
+         exfalso. clear IHHe.
+         unfold hoare_triple in Hhoare.
+         apply Hhoare in He.
+         + destruct He as [st' [C _] ]. inversion C.
+         + split; assumption.
+    Qed.
+    
+    (** Finally, state Hoare rules for [assert] and [assume] and use them
+        to prove a simple program correct.  Name your rules [hoare_assert]
+        and [hoare_assume]. *)
+    
+    (* FILL IN HERE *)
+    
+    (** Use your rules to prove the following triple. *)
+    
+    Example assert_assume_example:
+      {{True}}
+        assume (X = 1);
+        X := X + 1;
+        assert (X = 2)
+      {{True}}.
+    Proof.
+    (* FILL IN HERE *) Admitted.
+    
+    End HoareAssertAssume.
+    (** [] *)
+    
