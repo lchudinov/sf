b86cec0789eaf39e67592d4cccc1453e8a78068f
Leonty Chudinov <leonty.chudinov@gmail.com>
2023-10-11 08:26:00 +0500

    Up to SimplArith1

--- plf/Smallstep.v
+++ plf/Smallstep.v
@@ -9,3 +9,25 @@ From PLF Require Import Imp.
 Set Default Goal Selector "!".
 Definition FILL_IN_HERE := <{True}>.
 
+Inductive tm : Type :=
+  | C : nat -> tm (* Constant *)
+  | P : tm -> tm -> tm. (* Plus *)
+  
+Fixpoint evalF (t : tm) : nat :=
+  match t with
+  | C n => n
+  | P t1 t2 => evalF t1 + evalF t2
+  end.
+  
+Reserved Notation " t '==>' n " (at level 50, left associativity).
+
+Inductive eval : tm -> nat -> Prop :=
+  | E_Const : forall n, C n ==> n
+  | E_Plus : forall t1 t2 v1 v2,
+    t1 ==> v1 ->
+    t2 ==> v2 ->
+    P t1 t2 ==> (v1 + v2)
+where " t '==>' n " := (eval t n).
+
+Module SimpleArith1.
+    
