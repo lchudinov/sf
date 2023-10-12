b953506889ef266e73305b6f399e9c2ee95c60a9
Leonty Chudinov <leonty.chudinov@gmail.com>
2023-10-12 08:38:59 +0500

    deterministic step

--- plf/Smallstep.v
+++ plf/Smallstep.v
@@ -30,4 +30,105 @@ Inductive eval : tm -> nat -> Prop :=
 where " t '==>' n " := (eval t n).
 
 Module SimpleArith1.
-    
+
+Reserved Notation " t '-->' t' " (at level 40).
+
+Inductive step : tm -> tm -> Prop :=
+  | ST_PlusConstConst : forall v1 v2,
+    P (C v1) (C v2) --> C (v1 + v2)
+  | ST_Plus1 : forall t1 t1' t2,
+    t1 --> t1' ->
+    P t1 t2 --> P t1' t2
+  | ST_Plus2 : forall v1 t2 t2',
+    t2 --> t2' ->
+    P (C v1) t2 --> P (C v1) t2'
+  
+  where " t '-->' t' " := (step t t').
+
+Example test_step_1 :
+      P
+        (P (C 1) (C 3))
+        (P (C 2) (C 4))
+      -->
+      P
+        (C 4)
+        (P (C 2) (C 4)).
+Proof.
+  apply ST_Plus1. apply ST_PlusConstConst.
+Qed.
+
+Example test_step_2 :
+      P
+        (C 0)
+        (P
+          (C 2)
+          (P (C 1) (C 3)))
+      -->
+      P
+        (C 0)
+        (P
+          (C 2)
+          (C 4)).
+Proof.
+  apply ST_Plus2.
+  apply ST_Plus2.
+  apply ST_PlusConstConst.
+Qed.
+
+End SimpleArith1.
+
+
+Definition relation (X : Type) := X -> X -> Prop.
+
+Definition deterministic {X : Type} (R : relation X) :=
+  forall x y1 y2 : X, R x y1 -> R x y2 -> y1 = y2.
+Module SimpleArith2.
+Import SimpleArith1.
+
+Theorem step_deterministic:
+  deterministic step.
+Proof.
+  unfold deterministic. intros x y1 y2 Hy1 Hy2.
+  generalize dependent y2.
+  induction Hy1; intros y2 Hy2.
+  - inversion Hy2; subst.
+    + reflexivity.
+    + inversion H2.
+    + inversion H2.
+  - inversion Hy2; subst.
+    + inversion Hy1.
+    + apply IHHy1 in H2. rewrite H2. reflexivity.
+    + inversion Hy1.
+  - inversion Hy2; subst.
+    + inversion Hy1.
+    + inversion H2.
+    + apply IHHy1 in H2. rewrite H2. reflexivity.
+Qed.
+
+End SimpleArith2.
+
+Ltac solve_by_inverts n :=
+  match goal with | H : ?T |- _ =>
+  match type of T with Prop =>
+    solve [
+      inversion H;
+      match n with S (S (?n')) => subst; solve_by_inverts (S n') end ]
+  end end.
+
+Ltac solve_by_invert :=
+  solve_by_inverts 1.
+
+Module SimpleArith3.
+Import SimpleArith1.
+Theorem step_deterministic_alt: deterministic step.
+Proof.
+  intros x y1 y2 Hy1 Hy2.
+  generalize dependent y2.
+  induction Hy1; intros y2 Hy2;
+    inversion Hy2; subst; try solve_by_invert.
+  - reflexivity.
+  - apply IHHy1 in H2. rewrite H2. reflexivity.
+  - apply IHHy1 in H2. rewrite H2. reflexivity.
+Qed.
+
+End SimpleArith3.
