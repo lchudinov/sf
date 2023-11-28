98a34297d58f6e233f6ce864e165516f9c6b4b9c
Leonty Chudinov <leonty.chudinov@gmail.com>
2023-11-28 08:26:15 +0500

    progress almost done

--- plf/StlcProp.v
+++ plf/StlcProp.v
@@ -453,6 +453,127 @@ Proof.
     + apply T_Const.
   - apply T_Const.
 Qed.
+
+Lemma weakening : forall Gamma Gamma' t T,
+     includedin Gamma Gamma' ->
+     Gamma |-- t \in T ->
+     Gamma' |-- t \in T.
+Proof.
+  intros Gamma Gamma' t T H Ht.
+  generalize dependent Gamma'.
+  induction Ht; eauto using includedin_update.
+Qed.
+
+Lemma weakening_empty : forall Gamma t T,
+     empty |-- t \in T ->
+     Gamma |-- t \in T.
+Proof.
+  intros Gamma t T.
+  eapply weakening.
+  discriminate.
+Qed.
+  
+Lemma substitution_preserves_typing : forall Gamma x U t v T,
+  x |-> U ; Gamma |-- t \in T ->
+  empty |-- v \in U ->
+  Gamma |-- [x:=v]t \in T.
+Proof.
+  intros Gamma x U t v T Ht Hv.
+  generalize dependent Gamma. generalize dependent T.
+  induction t; intros T Gamma H;
+  (* in each case, we'll want to get at the derivation of H *)
+    inversion H; clear H; subst; simpl; eauto.
+  - (* var *)
+    rename s into y. destruct (eqb_spec x y); subst.
+    + (* x=y *)
+      rewrite update_eq in H2.
+      injection H2 as H2; subst.
+      apply weakening_empty. assumption.
+    + (* x<>y *)
+      apply T_Var. rewrite update_neq in H2; auto.
+  - (* abs *)
+    rename s into y, t into S.
+    destruct (eqb_spec x y); subst; apply T_Abs.
+    + (* x=y *)
+      rewrite update_shadow in H5. assumption.
+    + (* x<>y *)
+      apply IHt.
+      rewrite update_permute; auto.
+Qed.
+
+Theorem preservation : forall t t' T,
+  empty |-- t \in T ->
+  t --> t' ->
+  empty |-- t' \in T.
+Proof with eauto.
+  intros t t' T HT. generalize dependent t'.
+  remember empty as Gamma.
+  induction HT; intros t' HE; subst; try solve [inversion HE; subst; auto].
+  - (* T_App *)
+    inversion HE; subst...
+    (* Most of the cases are immediate by induction,
+       and eauto takes care of them *)
+    + (* ST_AppAbs *)
+      apply substitution_preserves_typing with T2...
+      inversion HT1...
+Qed.
+
+
+(*
+ Lemma canonical_forms_const : forall t,
+  empty |-- t \in Nat ->
+  value t ->
+  (t = <{true}>) \/ (t = <{false}>).
+Proof.
+  intros t HT HVal.
+  destruct HVal; auto.
+  inversion HT.
+Qed.
+ *)
+
+Lemma canonical_forms_fun : forall t T1 T2,
+  empty |-- t \in (T1 -> T2) ->
+  value t ->
+  exists x u, t = <{\x:T1, u}>.
+Proof.
+  intros t T1 T2 HT HVal.
+  destruct HVal as [x ? t1 |]; inversion HT; subst.
+  exists x, t1. reflexivity.
+Qed.
+
+Theorem progress : forall t T,
+  empty |-- t \in T ->
+  value t \/ exists t', t --> t'.
+Proof with eauto.
+  intros t T Ht.
+  remember empty as Gamma.
+  induction Ht; subst Gamma; auto.
+    (* auto solves all three cases in which t is a value *)
+  - (* T_Var *)
+    (* contradictory: variables cannot be typed in an empty context *)
+    discriminate H.
+  - (* T_App *)
+    (* t = t1 t2. Proced by cases on whether t1 is a value or steps... *)
+    right. destruct IHHt1...
+    + (* t1 is a value *)
+      destruct IHHt2...
+      * (* t2 is also a value *)
+        eapply canonical_forms_fun in Ht1; [|assumption].
+        destruct Ht1 as [x [t0 H1]]. subst.
+        exists (<{ [x:=t2]t0 }>)...
+      * (* t2 steps *)
+        destruct H0 as [t2' Hstp]. exists (<{ t1 t2' }>)...
+    + (* t1 steps *)
+      destruct H as [t1' Hstp]. exists (<{ t1' t2 }>)...
+  - (* T_Const*)
+    
+  - (* TIf *)
+    right. destruct IHHt1...
+    + (* t1 is a value *)
+      destruct (canonical_forms_bool t1); subst; eauto.
+    + (* t1 also steps *)
+      destruct H as [t1' Hstp]. exists (<{ if t1' then t2 else t3 }>)...
+Qed.
   
 End STLCArith.
 End STLCProp.
