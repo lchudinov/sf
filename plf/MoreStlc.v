29d49007bd139a52710eb14e1c01a2150c62f5e6
Leonty Chudinov <leonty.chudinov@gmail.com>
2023-12-08 08:31:24 +0500

    subst up to let

--- plf/MoreStlc.v
+++ plf/MoreStlc.v
@@ -5,3 +5,181 @@ From PLF Require Import Smallstep.
 From PLF Require Import Stlc.
 Set Default Goal Selector "!".
 
+Module STLCExtended.
+
+Inductive ty : Type :=
+  | Ty_Arrow : ty -> ty -> ty
+  | Ty_Nat : ty
+  | Ty_Sum : ty -> ty -> ty
+  | Ty_List : ty -> ty
+  | Ty_Unit : ty
+  | Ty_Prod : ty -> ty -> ty.
+  
+Inductive tm : Type :=
+  (* pure STLC *)
+  | tm_var : string -> tm
+  | tm_app : tm -> tm -> tm
+  | tm_abs : string -> ty -> tm -> tm
+  (* numbers *)
+  | tm_const : nat -> tm
+  | tm_succ : tm -> tm
+  | tm_pred : tm -> tm
+  | tm_mult : tm -> tm -> tm
+  | tm_if0 : tm -> tm -> tm -> tm
+  (* sums *)
+  | tm_inl : ty -> tm -> tm
+  | tm_inr : ty -> tm -> tm
+  | tm_case : tm -> string -> tm -> string -> tm -> tm (* i.e., case t0 of inl x1 => t1 | inr x2 => t2 *)
+  (* lists *)
+  | tm_nil : ty -> tm
+  | tm_cons : tm -> tm -> tm
+  | tm_lcase : tm -> tm -> string -> string -> tm -> tm (* i.e., case t1 of | nil => t2 | x::y => t3 *)
+  (* unit *)
+  | tm_unit: tm
+
+  (* You are going to be working on the following extensions: *)
+
+  (* pairs *)
+  | tm_pair : tm -> tm -> tm
+  | tm_fst : tm -> tm
+  | tm_snd : tm -> tm
+  (* let *)
+  | tm_let : string -> tm -> tm -> tm (* i.e., let x = t1 in t2 *)
+  (* fix *)
+  | tm_fix : tm -> tm
+  .
+  
+Definition x : string := "x".
+Definition y : string := "y".
+Definition z : string := "z".
+Hint Unfold x : core.
+Hint Unfold y : core.
+Hint Unfold z : core.
+Declare Custom Entry stlc_ty.
+Notation "<{ e }>" := e (e custom stlc at level 99).
+Notation "<{{ e }}>" := e (e custom stlc_ty at level 99).
+Notation "( x )" := x (in custom stlc, x at level 99).
+Notation "( x )" := x (in custom stlc_ty, x at level 99).
+Notation "x" := x (in custom stlc at level 0, x constr at level 0).
+Notation "x" := x (in custom stlc_ty at level 0, x constr at level 0).
+Notation "S -> T" := (Ty_Arrow S T) (in custom stlc_ty at level 50, right associativity).
+Notation "x y" := (tm_app x y) (in custom stlc at level 1, left associativity).
+Notation "\ x : t , y" :=
+  (tm_abs x t y) (in custom stlc at level 90, x at level 99,
+                     t custom stlc_ty at level 99,
+                     y custom stlc at level 99,
+                     left associativity).
+Coercion tm_var : string >-> tm.
+Notation "{ x }" := x (in custom stlc at level 1, x constr).
+Notation "'Nat'" := Ty_Nat (in custom stlc_ty at level 0).
+Notation "'succ' x" := (tm_succ x) (in custom stlc at level 0,
+                                     x custom stlc at level 0).
+Notation "'pred' x" := (tm_pred x) (in custom stlc at level 0,
+                                     x custom stlc at level 0).
+Notation "x * y" := (tm_mult x y) (in custom stlc at level 1,
+                                      left associativity).
+Notation "'if0' x 'then' y 'else' z" :=
+  (tm_if0 x y z) (in custom stlc at level 89,
+                    x custom stlc at level 99,
+                    y custom stlc at level 99,
+                    z custom stlc at level 99,
+                    left associativity).
+Coercion tm_const : nat >-> tm.
+Notation "S + T" :=
+  (Ty_Sum S T) (in custom stlc_ty at level 3, left associativity).
+Notation "'inl' T t" := (tm_inl T t) (in custom stlc at level 0,
+                                         T custom stlc_ty at level 0,
+                                         t custom stlc at level 0).
+Notation "'inr' T t" := (tm_inr T t) (in custom stlc at level 0,
+                                         T custom stlc_ty at level 0,
+                                         t custom stlc at level 0).
+Notation "'case' t0 'of' '|' 'inl' x1 '=>' t1 '|' 'inr' x2 '=>' t2" :=
+  (tm_case t0 x1 t1 x2 t2) (in custom stlc at level 89,
+                               t0 custom stlc at level 99,
+                               x1 custom stlc at level 99,
+                               t1 custom stlc at level 99,
+                               x2 custom stlc at level 99,
+                               t2 custom stlc at level 99,
+                               left associativity).
+Notation "X * Y" :=
+  (Ty_Prod X Y) (in custom stlc_ty at level 2, X custom stlc_ty, Y custom stlc_ty at level 0).
+Notation "( x ',' y )" := (tm_pair x y) (in custom stlc at level 0,
+                                                x custom stlc at level 99,
+                                                y custom stlc at level 99).
+Notation "t '.fst'" := (tm_fst t) (in custom stlc at level 1).
+Notation "t '.snd'" := (tm_snd t) (in custom stlc at level 1).
+Notation "'List' T" :=
+  (Ty_List T) (in custom stlc_ty at level 4).
+Notation "'nil' T" := (tm_nil T) (in custom stlc at level 0, T custom stlc_ty at level 0).
+Notation "h '::' t" := (tm_cons h t) (in custom stlc at level 2, right associativity).
+Notation "'case' t1 'of' '|' 'nil' '=>' t2 '|' x '::' y '=>' t3" :=
+  (tm_lcase t1 t2 x y t3) (in custom stlc at level 89,
+                              t1 custom stlc at level 99,
+                              t2 custom stlc at level 99,
+                              x constr at level 1,
+                              y constr at level 1,
+                              t3 custom stlc at level 99,
+                              left associativity).
+Notation "'Unit'" :=
+  (Ty_Unit) (in custom stlc_ty at level 0).
+Notation "'unit'" := tm_unit (in custom stlc at level 0).
+Notation "'let' x '=' t1 'in' t2" :=
+  (tm_let x t1 t2) (in custom stlc at level 0).
+Notation "'fix' t" := (tm_fix t) (in custom stlc at level 0).
+
+Reserved Notation "'[' x ':=' s ']' t" (in custom stlc at level 20, x constr).
+
+Fixpoint subst (x : string) (s : tm) (t : tm) : tm :=
+  match t with
+  (* pure STLC *)
+  | tm_var y =>
+      if String.eqb x y then s else t
+  | <{\y:T, t1}> =>
+      if String.eqb x y then t else <{\y:T, [x:=s] t1}>
+  | <{t1 t2}> =>
+      <{([x:=s] t1) ([x:=s] t2)}>
+  (* numbers *)
+  | tm_const _ =>
+      t
+  | <{succ t1}> =>
+      <{succ [x := s] t1}>
+  | <{pred t1}> =>
+      <{pred [x := s] t1}>
+  | <{t1 * t2}> =>
+      <{ ([x := s] t1) * ([x := s] t2)}>
+  | <{if0 t1 then t2 else t3}> =>
+    <{if0 [x := s] t1 then [x := s] t2 else [x := s] t3}>
+  (* sums *)
+  | <{inl T2 t1}> =>
+      <{inl T2 ( [x:=s] t1) }>
+  | <{inr T1 t2}> =>
+      <{inr T1 ( [x:=s] t2) }>
+  | <{case t0 of | inl y1 => t1 | inr y2 => t2}> =>
+      <{case ([x:=s] t0) of
+         | inl y1 => { if String.eqb x y1 then t1 else <{ [x:=s] t1 }> }
+         | inr y2 => {if String.eqb x y2 then t2 else <{ [x:=s] t2 }> } }>
+  (* lists *)
+  | <{nil _}> =>
+      t
+  | <{t1 :: t2}> =>
+      <{ ([x:=s] t1) :: [x:=s] t2 }>
+  | <{case t1 of | nil => t2 | y1 :: y2 => t3}> =>
+      <{case ( [x:=s] t1 ) of
+        | nil => [x:=s] t2
+        | y1 :: y2 =>
+        {if String.eqb x y1 then
+           t3
+         else if String.eqb x y2 then t3
+              else <{ [x:=s] t3 }> } }>
+  (* unit *)
+  | <{unit}> => <{unit}>
+  (* Complete the following cases. *)
+  (* pairs *)
+  | <{ ( t1, t2 ) }> => <{ (([x:=s] t1), ([x:=s] t2)) }>
+  (* let *)
+  (* FILL IN HERE *)
+  (* fix *)
+  (* FILL IN HERE *)
+  | _ => t (* ... and delete this line when you finish the exercise *)
+  end
+where "'[' x ':=' s ']' t" := (subst x s t) (in custom stlc).
