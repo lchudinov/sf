744852943ef5082333fea6c766c95c0711b420c6
Leonty Chudinov <leonty.chudinov@gmail.com>
2024-01-31 08:17:49 +0500

    finish records

--- plf/Records.v
+++ plf/Records.v
@@ -225,6 +225,8 @@ Proof.
   inversion H; subst; clear H.
   inversion H7.
 Qed.
+
+
 Example typing_nonexample_2 : forall y,
   ~ exists T,
     (y |-> A) |--
@@ -246,3 +248,248 @@ Proof.
   simpl in H5. inversion H5; subst; clear H5.
   clear H1 H3.
 
+  (* find out T1 *)
+  inversion H4; subst; clear H4.
+  inversion H6.
+Qed.
+
+Lemma wf_rcd_lookup : forall i T Ti,
+  well_formed_ty T ->
+  Tlookup i T = Some Ti ->
+  well_formed_ty Ti.
+Proof with eauto.
+  intros i T.
+  induction T; intros; try solve_by_invert.
+  - (* RCons *)
+    inversion H. subst. unfold Tlookup in H0.
+    destruct (String.eqb i s)...
+    inversion H0. subst...
+Qed.
+
+Lemma step_preserves_record_tm : forall tr tr',
+  record_tm tr ->
+  tr --> tr' ->
+  record_tm tr'.
+Proof.
+  intros tr tr' Hrt Hstp.
+  inversion Hrt; subst; inversion Hstp; subst; auto.
+Qed.
+
+Lemma has_type__wf : forall Gamma t T,
+  Gamma |-- t \in T -> well_formed_ty T.
+Proof with eauto.
+  intros Gamma t T Htyp.
+  induction Htyp...
+  - (* T_App *)
+    inversion IHHtyp1...
+  - (* T_Proj *)
+    eapply wf_rcd_lookup...
+Qed.
+
+Lemma lookup_field_in_value : forall v T i Ti,
+  value v ->
+  empty |-- v \in T ->
+  Tlookup i T = Some Ti ->
+  exists ti, tlookup i v = Some ti /\ empty |-- ti \in Ti.
+Proof with eauto.
+  intros v T i Ti Hval Htyp Hget.
+  remember empty as Gamma.
+  induction Htyp; subst; try solve_by_invert...
+  - (* T_RCons *)
+    simpl in Hget. simpl. destruct (String.eqb i i0).
+    + (* i is first *)
+      simpl. injection Hget as Hget. subst.
+      exists t...
+    + (* get tail *)
+      destruct IHHtyp2 as [vi [Hgeti Htypi] ]...
+      inversion Hval...
+Qed.
+
+Theorem progress : forall t T,
+     empty |-- t \in T ->
+     value t \/ exists t', t --> t'.
+Proof with eauto.
+  (* Theorem: Suppose empty |-- t : T.  Then either
+       1. t is a value, or
+       2. t --> t' for some t'.
+     Proof: By induction on the given typing derivation. *)
+  intros t T Ht.
+  remember empty as Gamma.
+  generalize dependent HeqGamma.
+  induction Ht; intros HeqGamma; subst.
+  - (* T_Var *)
+    (* The final rule in the given typing derivation cannot be
+       T_Var, since it can never be the case that
+       empty |-- x : T (since the context is empty). *)
+    inversion H.
+  - (* T_Abs *)
+    (* If the T_Abs rule was the last used, then
+       t = abs x T11 t12, which is a value. *)
+    left...
+  - (* T_App *)
+    (* If the last rule applied was T_App, then t = t1 t2,
+       and we know from the form of the rule that
+         empty |-- t1 : T1 -> T2
+         empty |-- t2 : T1
+       By the induction hypothesis, each of t1 and t2 either is a value
+       or can take a step. *)
+    right.
+    destruct IHHt1; subst...
+    + (* t1 is a value *)
+      destruct IHHt2; subst...
+      * (* t2 is a value *)
+      (* If both t1 and t2 are values, then we know that
+         t1 = abs x T11 t12, since abstractions are the only
+         values that can have an arrow type.  But
+         (abs x T11 t12) t2 --> [x:=t2]t12 by ST_AppAbs. *)
+        inversion H; subst; try solve_by_invert.
+        exists <{ [x:=t2]t0 }>...
+      * (* t2 steps *)
+        (* If t1 is a value and t2 --> t2', then
+           t1 t2 --> t1 t2' by ST_App2. *)
+        destruct H0 as [t2' Hstp]. exists <{ t1 t2' }>...
+    + (* t1 steps *)
+      (* Finally, If t1 --> t1', then t1 t2 --> t1' t2
+         by ST_App1. *)
+      destruct H as [t1' Hstp]. exists <{ t1' t2 }>...
+  - (* T_Proj *)
+    (* If the last rule in the given derivation is T_Proj, then
+       t = rproj t i and
+           empty |-- t : (TRcd Tr)
+       By the IH, t either is a value or takes a step. *)
+    right. destruct IHHt...
+    + (* rcd is value *)
+      (* If t is a value, then we may use lemma
+         lookup_field_in_value to show tlookup i t = Some ti
+         for some ti which gives us rproj i t --> ti by
+         ST_ProjRcd. *)
+      destruct (lookup_field_in_value _ _ _ _ H0 Ht H)
+        as [ti [Hlkup _] ].
+      exists ti...
+    + (* rcd_steps *)
+      (* On the other hand, if t --> t', then
+         rproj t i --> rproj t' i by ST_Proj1. *)
+      destruct H0 as [t' Hstp]. exists <{ t' --> i }>...
+  - (* T_RNil *)
+    (* If the last rule in the given derivation is T_RNil,
+       then t = trnil, which is a value. *)
+    left...
+  - (* T_RCons *)
+    (* If the last rule is T_RCons, then t = rcons i t tr and
+         empty |-- t : T
+         empty |-- tr : Tr
+       By the IH, each of t and tr either is a value or can
+       take a step. *)
+    destruct IHHt1...
+    + (* head is a value *)
+      destruct IHHt2; try reflexivity.
+      * (* tail is a value *)
+      (* If t and tr are both values, then rcons i t tr
+         is a value as well. *)
+        left...
+      * (* tail steps *)
+        (* If t is a value and tr --> tr', then
+           rcons i t tr --> rcons i t tr' by
+           ST_Rcd_Tail. *)
+        right. destruct H2 as [tr' Hstp].
+        exists <{ i := t :: tr'}>...
+    + (* head steps *)
+      (* If t --> t', then
+         rcons i t tr --> rcons i t' tr
+         by ST_Rcd_Head. *)
+      right. destruct H1 as [t' Hstp].
+      exists <{ i := t' :: tr }>...
+Qed.
+
+Lemma weakening : forall Gamma Gamma' t T,
+     includedin Gamma Gamma' ->
+     Gamma |-- t \in T ->
+     Gamma' |-- t \in T.
+Proof.
+  intros Gamma Gamma' t T H Ht.
+  generalize dependent Gamma'.
+  induction Ht; eauto using includedin_update.
+Qed.
+Lemma weakening_empty : forall Gamma t T,
+     empty |-- t \in T ->
+     Gamma |-- t \in T.
+Proof.
+  intros Gamma t T.
+  eapply weakening.
+  discriminate.
+Qed.
+
+Lemma substitution_preserves_typing : forall Gamma x U t v T,
+  (x |-> U ; Gamma) |-- t \in T ->
+  empty |-- v \in U ->
+  Gamma |-- [x:=v]t \in T.
+Proof.
+  intros Gamma x U t v T Ht Hv.
+  generalize dependent Gamma. generalize dependent T.
+  induction t; intros T Gamma H;
+  (* in each case, we'll want to get at the derivation of H *)
+    inversion H; clear H; subst; simpl; eauto.
+  - (* var *)
+    rename s into y. destruct (eqb_spec x y); subst.
+    + (* x=y *)
+      rewrite update_eq in H1.
+      injection H1 as H1; subst.
+      apply weakening_empty. assumption.
+    + (* x<>y *)
+      apply T_Var. rewrite update_neq in H1; auto. assumption.
+  - (* abs *)
+    rename s into y, t into T.
+    destruct (eqb_spec x y); subst; apply T_Abs; try assumption.
+    + (* x=y *)
+      rewrite update_shadow in H5. assumption.
+    + (* x<>y *)
+      apply IHt.
+      rewrite update_permute; auto.
+  - (* rcons *) (* <=== only new case compared to pure STLC *)
+     apply T_RCons; eauto.
+     inversion H7; subst; simpl; auto.
+Qed.
+
+Theorem preservation : forall t t' T,
+  empty |-- t \in T ->
+  t --> t' ->
+  empty |-- t' \in T.
+Proof with eauto.
+  intros t t' T HT. generalize dependent t'.
+  remember empty as Gamma.
+  induction HT;
+       intros t' HE; subst;
+       try solve [inversion HE; subst; auto].
+  - (* T_App *)
+    inversion HE; subst...
+    + (* ST_AppAbs *)
+      apply substitution_preserves_typing with T1...
+      inversion HT1...
+  - (* T_Proj *) (* <=== new case compared to pure STLC *)
+    (* If the last rule was T_Proj, then t = rproj t1 i.
+       Two rules could have caused t --> t': T_Proj1 and
+       T_ProjRcd.  The typing of t' follows from the IH
+       in the former case, so we only consider T_ProjRcd.
+
+       Here we have that t is a record value.  Since rule
+       T_Proj was used, we know empty |-- t \in Tr and
+       Tlookup i Tr = Some Ti for some i and Tr.
+       We may therefore apply lemma lookup_field_in_value
+       to find the record element this projection steps to. *)
+    inversion HE; subst...
+    destruct (lookup_field_in_value _ _ _ _ H2 HT H)
+      as [vi [Hget Htyp] ].
+    rewrite H4 in Hget. injection Hget as Hget. subst...
+  - (* T_RCons *) (* <=== new case compared to pure STLC *)
+    (* If the last rule was T_RCons, then t = rcons i t tr
+       for some i, t and tr such that record_tm tr.  If
+       the step is by ST_Rcd_Head, the result is immediate by
+       the IH.  If the step is by ST_Rcd_Tail, tr --> tr2'
+       for some tr2' and we must also use lemma step_preserves_record_tm
+       to show record_tm tr2'. *)
+    inversion HE; subst...
+    apply T_RCons... eapply step_preserves_record_tm...
+Qed.
+
+End STLCExtendedRecords.
+  
