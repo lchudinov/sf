3910817286b5e127b8cb9020f8c433f28e4f26c3
Leonty Chudinov <leonty.chudinov@gmail.com>
2024-02-13 08:43:45 +0500

    Up to subtyping

--- plf/RecordSub.v
+++ plf/RecordSub.v
@@ -45,3 +45,166 @@ Notation "'nil'" := (Ty_RNil) (in custom stlc_ty).
 Notation "'nil'" := (tm_rnil) (in custom stlc).
 Notation "o --> l" := (tm_rproj o l) (in custom stlc at level 0).
 Notation "'Top'" := (Ty_Top) (in custom stlc_ty at level 0).
+
+Inductive record_ty : ty -> Prop :=
+  | RTnil :
+        record_ty <{{ nil }}>
+  | RTcons : forall i T1 T2,
+        record_ty <{{ i : T1 :: T2 }}>.
+Inductive record_tm : tm -> Prop :=
+  | rtnil :
+        record_tm <{ nil }>
+  | rtcons : forall i t1 t2,
+        record_tm <{ i := t1 :: t2 }>.
+Inductive well_formed_ty : ty -> Prop :=
+  | wfTop :
+        well_formed_ty <{{ Top }}>
+  | wfBase : forall (i : string),
+        well_formed_ty <{{ Base i }}>
+  | wfArrow : forall T1 T2,
+        well_formed_ty T1 ->
+        well_formed_ty T2 ->
+        well_formed_ty <{{ T1 -> T2 }}>
+  | wfRNil :
+        well_formed_ty <{{ nil }}>
+  | wfRCons : forall i T1 T2,
+        well_formed_ty T1 ->
+        well_formed_ty T2 ->
+        record_ty T2 ->
+        well_formed_ty <{{ i : T1 :: T2 }}>.
+Hint Constructors record_ty record_tm well_formed_ty : core.
+
+Reserved Notation "'[' x ':=' s ']' t" (in custom stlc at level 20, x constr).
+Fixpoint subst (x : string) (s : tm) (t : tm) : tm :=
+  match t with
+  | tm_var y =>
+      if String.eqb x y then s else t
+  | <{\y:T, t1}> =>
+      if String.eqb x y then t else <{\y:T, [x:=s] t1}>
+  | <{t1 t2}> =>
+      <{([x:=s] t1) ([x:=s] t2)}>
+  | <{ t1 --> i }> =>
+      <{ ( [x := s] t1) --> i }>
+  | <{ nil }> =>
+      <{ nil }>
+  | <{ i := t1 :: tr }> =>
+     <{ i := [x := s] t1 :: ( [x := s] tr) }>
+  end
+
+where "'[' x ':=' s ']' t" := (subst x s t) (in custom stlc).
+
+Inductive value : tm -> Prop :=
+  | v_abs : forall x T2 t1,
+      value <{ \ x : T2, t1 }>
+  | v_rnil : value <{ nil }>
+  | v_rcons : forall i v1 vr,
+      value v1 ->
+      value vr ->
+      value <{ i := v1 :: vr }>.
+Hint Constructors value : core.
+
+Fixpoint Tlookup (i:string) (Tr:ty) : option ty :=
+  match Tr with
+  | <{{ i' : T :: Tr' }}> =>
+      if String.eqb i i' then Some T else Tlookup i Tr'
+  | _ => None
+  end.
+
+Fixpoint tlookup (i:string) (tr:tm) : option tm :=
+  match tr with
+  | <{ i' := t :: tr' }> =>
+      if String.eqb i i' then Some t else tlookup i tr'
+  | _ => None
+  end.
+
+Inductive step : tm -> tm -> Prop :=
+  | ST_AppAbs : forall x T2 t1 v2,
+         value v2 ->
+         <{(\x:T2, t1) v2}> --> <{ [x:=v2]t1 }>
+  | ST_App1 : forall t1 t1' t2,
+         t1 --> t1' ->
+         <{t1 t2}> --> <{t1' t2}>
+  | ST_App2 : forall v1 t2 t2',
+         value v1 ->
+         t2 --> t2' ->
+         <{v1 t2}> --> <{v1 t2'}>
+  | ST_Proj1 : forall t1 t1' i,
+        t1 --> t1' ->
+        <{ t1 --> i }> --> <{ t1' --> i }>
+  | ST_ProjRcd : forall tr i vi,
+        value tr ->
+        tlookup i tr = Some vi ->
+        <{ tr --> i }> --> vi
+  | ST_Rcd_Head : forall i t1 t1' tr2,
+        t1 --> t1' ->
+        <{ i := t1 :: tr2 }> --> <{ i := t1' :: tr2 }>
+  | ST_Rcd_Tail : forall i v1 tr2 tr2',
+        value v1 ->
+        tr2 --> tr2' ->
+        <{ i := v1 :: tr2 }> --> <{ i := v1 :: tr2' }>
+
+where "t '-->' t'" := (step t t').
+Hint Constructors step : core.
+
+Reserved Notation "T '<:' U" (at level 40).
+
+Inductive subtype : ty -> ty -> Prop :=
+  (* Subtyping between proper types *)
+  | S_Refl : forall T,
+    well_formed_ty T ->
+    T <: T
+  | S_Trans : forall S U T,
+    S <: U ->
+    U <: T ->
+    S <: T
+  | S_Top : forall S,
+    well_formed_ty S ->
+    S <: <{{ Top }}>
+  | S_Arrow : forall S1 S2 T1 T2,
+    T1 <: S1 ->
+    S2 <: T2 ->
+    <{{ S1 -> S2 }}> <: <{{ T1 -> T2 }}>
+  (* Subtyping between record types *)
+  | S_RcdWidth : forall i T1 T2,
+    well_formed_ty <{{ i : T1 :: T2 }}> ->
+    <{{ i : T1 :: T2 }}> <: <{{ nil }}>
+  | S_RcdDepth : forall i S1 T1 Sr2 Tr2,
+    S1 <: T1 ->
+    Sr2 <: Tr2 ->
+    record_ty Sr2 ->
+    record_ty Tr2 ->
+    <{{ i : S1 :: Sr2 }}> <: <{{ i : T1 :: Tr2 }}>
+  | S_RcdPerm : forall i1 i2 T1 T2 Tr3,
+    well_formed_ty <{{ i1 : T1 :: i2 : T2 :: Tr3 }}> ->
+    i1 <> i2 ->
+       <{{ i1 : T1 :: i2 : T2 :: Tr3 }}>
+    <: <{{ i2 : T2 :: i1 : T1 :: Tr3 }}>
+
+where "T '<:' U" := (subtype T U).
+
+Hint Constructors subtype : core.
+
+
+Module Examples.
+Open Scope string_scope.
+Notation x := "x".
+Notation y := "y".
+Notation z := "z".
+Notation j := "j".
+Notation k := "k".
+Notation i := "i".
+Notation A := <{{ Base "A" }}>.
+Notation B := <{{ Base "B" }}>.
+Notation C := <{{ Base "C" }}>.
+Definition TRcd_j :=
+  <{{ j : (B -> B) :: nil }}>. (* {j:B->B} *)
+Definition TRcd_kj :=
+  <{{ k : (A -> A) :: TRcd_j }}>. (* {k:C->C,j:B->B} *)
+Example subtyping_example_0 :
+  <{{ C -> TRcd_kj }}> <: <{{ C -> nil }}>.
+Proof.
+  apply S_Arrow.
+    apply S_Refl. auto.
+    unfold TRcd_kj, TRcd_j. apply S_RcdWidth; auto.
+Qed.
+
