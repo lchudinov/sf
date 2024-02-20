d67a17c9ffbbcd465792f274a05eae0178b8a941
Leonty Chudinov <leonty.chudinov@gmail.com>
2024-02-20 08:27:10 +0500

    Norm unfinished because it's optional

--- plf/Norm.v
+++ plf/Norm.v
@@ -4,3 +4,365 @@ From Coq Require Import Strings.String.
 From PLF Require Import Maps.
 From PLF Require Import Smallstep.
 Hint Constructors multi : core.
+
+Inductive ty : Type :=
+  | Ty_Bool : ty
+  | Ty_Arrow : ty -> ty -> ty
+  | Ty_Prod : ty -> ty -> ty
+.
+Inductive tm : Type :=
+    (* pure STLC *)
+  | tm_var : string -> tm
+  | tm_app : tm -> tm -> tm
+  | tm_abs : string -> ty -> tm -> tm
+    (* booleans *)
+  | tm_true : tm
+  | tm_false : tm
+  | tm_if : tm -> tm -> tm -> tm
+    (* pairs *)
+  | tm_pair : tm -> tm -> tm
+  | tm_fst : tm -> tm
+  | tm_snd : tm -> tm.
+Declare Custom Entry stlc.
+Notation "<{ e }>" := e (e custom stlc at level 99).
+Notation "( x )" := x (in custom stlc, x at level 99).
+Notation "x" := x (in custom stlc at level 0, x constr at level 0).
+Notation "S -> T" := (Ty_Arrow S T) (in custom stlc at level 50, right associativity).
+Notation "x y" := (tm_app x y) (in custom stlc at level 1, left associativity).
+Notation "\ x : t , y" :=
+  (tm_abs x t y) (in custom stlc at level 90, x at level 99,
+                     t custom stlc at level 99,
+                     y custom stlc at level 99,
+                     left associativity).
+Coercion tm_var : string >-> tm.
+Notation "{ x }" := x (in custom stlc at level 1, x constr).
+Notation "'Bool'" := Ty_Bool (in custom stlc at level 0).
+Notation "'if' x 'then' y 'else' z" :=
+  (tm_if x y z) (in custom stlc at level 89,
+                    x custom stlc at level 99,
+                    y custom stlc at level 99,
+                    z custom stlc at level 99,
+                    left associativity).
+Notation "'true'" := true (at level 1).
+Notation "'true'" := tm_true (in custom stlc at level 0).
+Notation "'false'" := false (at level 1).
+Notation "'false'" := tm_false (in custom stlc at level 0).
+Notation "X * Y" :=
+  (Ty_Prod X Y) (in custom stlc at level 2, X custom stlc, Y custom stlc at level 0).
+Notation "( x ',' y )" := (tm_pair x y) (in custom stlc at level 0,
+                                                x custom stlc at level 99,
+                                                y custom stlc at level 99).
+Notation "t '.fst'" := (tm_fst t) (in custom stlc at level 1).
+Notation "t '.snd'" := (tm_snd t) (in custom stlc at level 1).
+
+Reserved Notation "'[' x ':=' s ']' t" (in custom stlc at level 20, x constr).
+Fixpoint subst (x : string) (s : tm) (t : tm) : tm :=
+  match t with
+  | tm_var y =>
+      if String.eqb x y then s else t
+  | <{ \ y : T, t1 }> =>
+      if String.eqb x y then t else <{ \y:T, [x:=s] t1 }>
+  | <{t1 t2}> =>
+      <{ ([x:=s]t1) ([x:=s]t2)}>
+  | <{true}> =>
+      <{true}>
+  | <{false}> =>
+      <{false}>
+  | <{if t1 then t2 else t3}> =>
+      <{if ([x:=s] t1) then ([x:=s] t2) else ([x:=s] t3)}>
+  | <{(t1, t2)}> =>
+      <{( ([x:=s] t1), ([x:=s] t2) )}>
+  | <{t0.fst}> =>
+      <{ ([x:=s] t0).fst}>
+  | <{t0.snd}> =>
+      <{ ([x:=s] t0).snd}>
+  end
+  where "'[' x ':=' s ']' t" := (subst x s t) (in custom stlc).
+
+Inductive value : tm -> Prop :=
+  | v_abs : forall x T2 t1,
+      value <{\x:T2, t1}>
+  | v_true :
+      value <{true}>
+  | v_false :
+      value <{false}>
+  | v_pair : forall v1 v2,
+      value v1 ->
+      value v2 ->
+      value <{(v1, v2)}>.
+
+Hint Constructors value : core.
+Reserved Notation "t '-->' t'" (at level 40).
+Inductive step : tm -> tm -> Prop :=
+  | ST_AppAbs : forall x T2 t1 v2,
+         value v2 ->
+         <{(\x:T2, t1) v2}> --> <{ [x:=v2]t1 }>
+  | ST_App1 : forall t1 t1' t2,
+         t1 --> t1' ->
+         <{t1 t2}> --> <{t1' t2}>
+  | ST_App2 : forall v1 t2 t2',
+         value v1 ->
+         t2 --> t2' ->
+         <{v1 t2}> --> <{v1 t2'}>
+  | ST_IfTrue : forall t1 t2,
+      <{if true then t1 else t2}> --> t1
+  | ST_IfFalse : forall t1 t2,
+      <{if false then t1 else t2}> --> t2
+  | ST_If : forall t1 t1' t2 t3,
+      t1 --> t1' ->
+      <{if t1 then t2 else t3}> --> <{if t1' then t2 else t3}>
+  | ST_Pair1 : forall t1 t1' t2,
+        t1 --> t1' ->
+        <{ (t1,t2) }> --> <{ (t1' , t2) }>
+  | ST_Pair2 : forall v1 t2 t2',
+        value v1 ->
+        t2 --> t2' ->
+        <{ (v1, t2) }> --> <{ (v1, t2') }>
+  | ST_Fst1 : forall t0 t0',
+        t0 --> t0' ->
+        <{ t0.fst }> --> <{ t0'.fst }>
+  | ST_FstPair : forall v1 v2,
+        value v1 ->
+        value v2 ->
+        <{ (v1,v2).fst }> --> v1
+  | ST_Snd1 : forall t0 t0',
+        t0 --> t0' ->
+        <{ t0.snd }> --> <{ t0'.snd }>
+  | ST_SndPair : forall v1 v2,
+        value v1 ->
+        value v2 ->
+        <{ (v1,v2).snd }> --> v2
+
+where "t '-->' t'" := (step t t').
+Hint Constructors step : core.
+Notation multistep := (multi step).
+Notation "t1 '-->*' t2" := (multistep t1 t2) (at level 40).
+Notation step_normal_form := (normal_form step).
+Lemma value__normal : forall t, value t -> step_normal_form t.
+Proof with eauto.
+  intros t H; induction H; intros [t' ST]; inversion ST...
+Qed.
+
+Definition context := partial_map ty.
+Reserved Notation "Gamma '|--' t '\in' T" (at level 40,
+                                          t custom stlc, T custom stlc at level 0).
+Inductive has_type : context -> tm -> ty -> Prop :=
+  (* Same as before: *)
+  (* pure STLC *)
+  | T_Var : forall Gamma x T1,
+      Gamma x = Some T1 ->
+      Gamma |-- x \in T1
+  | T_Abs : forall Gamma x T1 T2 t1,
+      (x |-> T2 ; Gamma) |-- t1 \in T1 ->
+      Gamma |-- \x:T2, t1 \in (T2 -> T1)
+  | T_App : forall T1 T2 Gamma t1 t2,
+      Gamma |-- t1 \in (T2 -> T1) ->
+      Gamma |-- t2 \in T2 ->
+      Gamma |-- t1 t2 \in T1
+  | T_True : forall Gamma,
+       Gamma |-- true \in Bool
+  | T_False : forall Gamma,
+       Gamma |-- false \in Bool
+  | T_If : forall t1 t2 t3 T1 Gamma,
+      Gamma |-- t1 \in Bool ->
+      Gamma |-- t2 \in T1 ->
+      Gamma |-- t3 \in T1 ->
+      Gamma |-- if t1 then t2 else t3 \in T1
+  | T_Pair : forall Gamma t1 t2 T1 T2,
+      Gamma |-- t1 \in T1 ->
+      Gamma |-- t2 \in T2 ->
+      Gamma |-- (t1, t2) \in (T1 * T2)
+  | T_Fst : forall Gamma t0 T1 T2,
+      Gamma |-- t0 \in (T1 * T2) ->
+      Gamma |-- t0.fst \in T1
+  | T_Snd : forall Gamma t0 T1 T2,
+      Gamma |-- t0 \in (T1 * T2) ->
+      Gamma |-- t0.snd \in T2
+
+where "Gamma '|--' t '\in' T" := (has_type Gamma t T).
+Hint Constructors has_type : core.
+Hint Extern 2 (has_type _ (app _ _) _) => eapply T_App; auto : core.
+Hint Extern 2 (_ = _) => compute; reflexivity : core.
+
+Lemma weakening : forall Gamma Gamma' t T,
+     includedin Gamma Gamma' ->
+     Gamma |-- t \in T ->
+     Gamma' |-- t \in T.
+Proof.
+  intros Gamma Gamma' t T H Ht.
+  generalize dependent Gamma'.
+  induction Ht; eauto using includedin_update.
+Qed.
+Lemma weakening_empty : forall Gamma t T,
+     empty |-- t \in T ->
+     Gamma |-- t \in T.
+Proof.
+  intros Gamma t T.
+  eapply weakening.
+  discriminate.
+Qed.
+
+Lemma substitution_preserves_typing : forall Gamma x U t v T,
+  (x |-> U ; Gamma) |-- t \in T ->
+  empty |-- v \in U ->
+  Gamma |-- [x:=v]t \in T.
+Proof with eauto.
+  intros Gamma x U t v T Ht Hv.
+  generalize dependent Gamma. generalize dependent T.
+  induction t; intros T Gamma H;
+  (* in each case, we'll want to get at the derivation of H *)
+    inversion H; clear H; subst; simpl; eauto.
+  - (* var *)
+    rename s into y. destruct (eqb_spec x y); subst.
+    + (* x=y *)
+      rewrite update_eq in H2.
+      injection H2 as H2; subst.
+      apply weakening_empty. assumption.
+    + (* x<>y *)
+      apply T_Var. rewrite update_neq in H2; auto.
+  - (* abs *)
+    rename s into y, t into S.
+    destruct (eqb_spec x y); subst; apply T_Abs.
+    + (* x=y *)
+      rewrite update_shadow in H5. assumption.
+    + (* x<>y *)
+      apply IHt.
+      rewrite update_permute; auto.
+Qed.
+
+Theorem preservation : forall t t' T,
+empty |-- t \in T ->
+t --> t' ->
+empty |-- t' \in T.
+Proof with eauto.
+intros t t' T HT. generalize dependent t'.
+remember empty as Gamma.
+induction HT;
+intros t' HE; subst; inversion HE; subst...
+- (* T_App *)
+inversion HE; subst...
++ (* ST_AppAbs *)
+ apply substitution_preserves_typing with T2...
+ inversion HT1...
+- (* T_Fst *)
+inversion HT...
+- (* T_Snd *)
+inversion HT...
+Qed.
+
+Inductive appears_free_in : string -> tm -> Prop :=
+  | afi_var : forall (x : string),
+      appears_free_in x <{ x }>
+  | afi_app1 : forall x t1 t2,
+      appears_free_in x t1 -> appears_free_in x <{ t1 t2 }>
+  | afi_app2 : forall x t1 t2,
+      appears_free_in x t2 -> appears_free_in x <{ t1 t2 }>
+  | afi_abs : forall x y T11 t12,
+        y <> x ->
+        appears_free_in x t12 ->
+        appears_free_in x <{ \y : T11, t12 }>
+  (* booleans *)
+  | afi_test0 : forall x t0 t1 t2,
+      appears_free_in x t0 ->
+      appears_free_in x <{ if t0 then t1 else t2 }>
+  | afi_test1 : forall x t0 t1 t2,
+      appears_free_in x t1 ->
+      appears_free_in x <{ if t0 then t1 else t2 }>
+  | afi_test2 : forall x t0 t1 t2,
+      appears_free_in x t2 ->
+      appears_free_in x <{ if t0 then t1 else t2 }>
+  (* pairs *)
+  | afi_pair1 : forall x t1 t2,
+      appears_free_in x t1 ->
+      appears_free_in x <{ (t1, t2) }>
+  | afi_pair2 : forall x t1 t2,
+      appears_free_in x t2 ->
+      appears_free_in x <{ (t1 , t2) }>
+  | afi_fst : forall x t,
+      appears_free_in x t ->
+      appears_free_in x <{ t.fst }>
+  | afi_snd : forall x t,
+      appears_free_in x t ->
+      appears_free_in x <{ t.snd }>
+.
+Hint Constructors appears_free_in : core.
+
+Definition closed (t:tm) :=
+  forall x, ~ appears_free_in x t.
+Lemma context_invariance : forall Gamma Gamma' t S,
+     Gamma |-- t \in S ->
+     (forall x, appears_free_in x t -> Gamma x = Gamma' x) ->
+     Gamma' |-- t \in S.
+Proof.
+  intros.
+  generalize dependent Gamma'.
+  induction H; intros; eauto 12.
+  - (* T_Var *)
+    apply T_Var. rewrite <- H0; auto.
+  - (* T_Abs *)
+    apply T_Abs.
+    apply IHhas_type. intros x1 Hafi.
+    (* the only tricky step... *)
+    destruct (eqb_spec x x1); subst.
+    + rewrite update_eq.
+      rewrite update_eq.
+      reflexivity.
+    + rewrite update_neq; [| assumption].
+      rewrite update_neq; [| assumption].
+      auto.
+Qed.
+
+(* A handy consequence of eqb_neq. *)
+Theorem false_eqb_string : forall x y : string,
+   x <> y -> String.eqb x y = false.
+Proof.
+  intros x y. rewrite String.eqb_neq.
+  intros H. apply H. Qed.
+Lemma free_in_context : forall x t T Gamma,
+   appears_free_in x t ->
+   Gamma |-- t \in T ->
+   exists T', Gamma x = Some T'.
+Proof with eauto.
+  intros x t T Gamma Hafi Htyp.
+  induction Htyp; inversion Hafi; subst...
+  - (* T_Abs *)
+    destruct IHHtyp as [T' Hctx]... exists T'.
+    unfold update, t_update in Hctx.
+    rewrite false_eqb_string in Hctx...
+Qed.
+
+Corollary typable_empty__closed : forall t T,
+    empty |-- t \in T ->
+    closed t.
+Proof.
+  intros. unfold closed. intros x H1.
+  destruct (free_in_context _ _ _ _ H1 H) as [T' C].
+  discriminate C.
+Qed.
+  
+Ltac solve_by_value_nf :=
+  match goal with | H : value ?v, H' : ?v --> ?v' |- _ =>
+  exfalso; apply value__normal in H; eauto
+  end.
+Lemma step_deterministic :
+   deterministic step.
+Proof with eauto.
+   unfold deterministic.
+   intros t t' t'' E1 E2.
+   generalize dependent t''.
+   induction E1; intros t'' E2; inversion E2; subst; clear E2;
+   try solve_by_invert; try f_equal; try solve_by_value_nf; eauto.
+   - inversion E1; subst; solve_by_value_nf.
+   - inversion H2; subst; solve_by_value_nf.
+   - inversion E1; subst; solve_by_value_nf.
+   - inversion H2; subst; solve_by_value_nf.
+Qed.
+
+Definition halts (t:tm) : Prop := exists t', t -->* t' /\ value t'.
+Lemma value_halts : forall v, value v -> halts v.
+Proof.
+  intros v H. unfold halts.
+  exists v. split.
+  - auto.
+  - auto.
+Qed.
